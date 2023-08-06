ea1d061a0c351d7d21389acf052c900ca4c2081d
s7manth <syalamarty002@gmail.com>
2023-08-06 13:30:17 +0800

    solve problem 16

--- src/bin/p16.rs
+++ src/bin/p16.rs
@@ -1,5 +1,33 @@
 // Problem 16: Power Digit Sum 
 
+fn power_digit_sum(number: usize, exponent: usize) -> usize {
+  if exponent == 0 {
+    return 1;
+  }
+
+  let mut arr = vec![];
+  arr.push(1);
+
+  for _ in 0..exponent {
+    let mut carry: usize = 0;
+
+    for d in arr.iter_mut() {
+      let product = *d * number + carry;
+      *d = product % 10;
+      carry = product / 10;
+    }
+
+    while carry != 0 {
+      arr.push(carry % 10);
+      carry /= 10;
+    }
+  }
+
+  arr
+    .iter()
+    .fold(0, |acc, x| acc + x)
+}
+
 fn main() {
-  
+  println!("{}", power_digit_sum(2, 1000));
 }
